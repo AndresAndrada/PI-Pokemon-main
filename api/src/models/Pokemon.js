5385543cc940432f5f7488ff71ec324ce34908d3
AndresAndrada <andresandrada1994@gmail.com>
2022-12-05 19:14:17 -0300

    Modulos creados sin modularizar, creada base de datos

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,54 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      default: DataTypes.UUIDV4, // designa un id que lo diferencia de la api
+      primaryKey: true // unico id
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false, // no sea null
     },
+    vida: {
+      type: DataTypes.STRING,
+      // unique: true // un unico valor,
+    },
+    ataque: {
+      type: DataTypes.STRING,
+    },
+    defensa: {
+      type: DataTypes.STRING,
+    },
+    velocidad: {
+      type: DataTypes.INTEGER
+    },
+    altura: {
+      type: DataTypes.INTEGER
+    },
+    peso: {
+      type: DataTypes.INTEGER
+    },
+    
+  },
+  {
+    timestamps: false,
   });
+
+  sequelize.define('tipo', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      default: DataTypes.UUIDV4
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false, // no sea null
+    }
+  },
+  {
+    timestamps: false,
+  })
 };
