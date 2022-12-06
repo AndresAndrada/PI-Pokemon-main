35ed94625ef2f54d7730ccdecaed1a21b1309fd2
AndresAndrada <andresandrada1994@gmail.com>
2022-12-05 21:36:54 -0300

    Modulos creados sin modularizar, creada base de datos

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const axios = require('axios');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,13 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+// router.use('/pokemons', pokeApi);
+// router.use('/types', pokeType);
+
+// router.get('/pokemons', (req, res) => {
+//     res.send('Hola')
+// })
 
 
 module.exports = router;
