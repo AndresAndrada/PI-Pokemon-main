b1db05114894a6b52ab710c313602006b91a4602
AndresAndrada <andresandrada1994@gmail.com>
2022-12-06 13:04:48 -0300

    Axios andando en el 1er get a la api, tratar de trer la data necesaria

--- api/src/controllers/poke.controller.js
+++ api/src/controllers/poke.controller.js
@@ -1,8 +1,24 @@
 // CONTORLADORES DE LA RUTA /pokemons
+const axios = require('axios');
+const { response } = require('express');
+const { name } = require('../app');
+// const { Pokemon } = require('../db')
 
-const getAllApi = (req, res, next) => {
+const getAllApi = async (req, res, next) => {
     try {
-        res.send('getAll')
+        // res.send('hola')
+        const pedido = await axios.get('https://pokeapi.co/api/v2/pokemon')
+        if(pedido) {
+        //     const character = pedido.data.map(pk => {
+        //         return {
+        //             name: pk.name
+
+        //         } 
+        //     })
+            res.send(pedido.data)
+        } else {
+            res.json({message: 'Algo salio mal'})
+        }
     } catch (error) {
         next(error)
     }
